8e6ddbee69f2728bdd80d13d0c7d1d7aefc3a8d8
Mayank Garg <mayank11065@iiitd.ac.in>
2016-07-31 13:37:20 +0530

    Instanceof, Iterator
    Questions on Instanceof, Iterator concept under Java -> OOP

---
+++ pkgJava/IteratorDemo.java
@@ -0,0 +1,42 @@
+package pkgJava;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class IteratorDemo {
+
+	static Iterator<Object> func(ArrayList<Object> mylist){
+	      Iterator<Object> it=mylist.iterator();
+	      while(it.hasNext()){
+	         Object element = it.next();
+	         if(element.equals("###"))
+	            break;
+	      }
+	      return it;
+	      
+	   }
+	
+	public static void main(String[] args) {
+		ArrayList<Object> mylist = new ArrayList<Object>();
+	      Scanner sc = new Scanner(System.in);
+	      int n = sc.nextInt();
+	      int m = sc.nextInt();
+	      for(int i = 0;i<n;i++){
+	         mylist.add(sc.nextInt());
+	      }
+	      
+	      mylist.add("###");
+	      for(int i=0;i<m;i++){
+	         mylist.add(sc.next());
+	      }
+	      sc.close();
+	      
+	      Iterator<Object> it=func(mylist);
+	      while(it.hasNext()){
+	         Object element = it.next();
+	         System.out.println((String)element);
+	      }
+	}
+
+}
