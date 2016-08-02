723f8bf06b128298662077f28248a753fd549dc6
Mayank Garg <mayank11065@iiitd.ac.in>
2016-08-02 20:11:59 +0530

    Variable Arguments
    Java varargs concept under Java -> Advanced

---
+++ pkgJava/VarargsDemo.java
@@ -0,0 +1,70 @@
+package pkgJava;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Add{
+
+	public void add(int... n) {
+		// TODO Auto-generated method stub
+		String series = Integer.toString(n[0]);
+		int sum = n[0];
+		for(int i =1;i< n.length-1;i++)
+		{
+			series = series + "+" + Integer.toString(n[i]);
+			sum += n[i];
+		}
+		
+		series += "+"+n[n.length-1];
+		sum += n[n.length-1];
+		
+		System.out.println(series + "=" + sum);
+	}
+	
+}
+public class VarargsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+	}
+
+}
