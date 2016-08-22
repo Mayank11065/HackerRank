0ae11713a4e11b1685ea5b5825a22153cf03fadf
Mayank Garg <mayank11065@iiitd.ac.in>
2016-08-22 16:17:19 +0530

    Arithmatic Operator, Division
    Arithmatic Operator, Division Demo in Python Domain

---
+++ Python/Introduction/arithematicOperators.py
@@ -0,0 +1,8 @@
+import sys
+
+N1 = int(input().strip())
+N2 = int(input().strip())
+
+print(N1+N2)
+print(N1-N2)
+print(N1*N2)
---
+++ Python/Introduction/division.py
@@ -0,0 +1,5 @@
+N1 = int(input().strip())
+N2 = int(input().strip())
+
+print(N1//N2) #Gives integer division (4//3 -> 1)
+print(N1/N2) #Results float division (4/3 -> 1.333)
