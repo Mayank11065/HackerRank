483f688aa33369ddc254f62cf1f5e19c09086a62
Mayank Garg <mayank11065@iiitd.ac.in>
2016-08-27 14:58:33 +0530

    Swap Case
    swap all cases in a String

---
+++ Python/Strings/swap_case.py
@@ -0,0 +1,14 @@
+str = input()
+
+#Long way to change case of string (if upper -> Lower and vice versa)
+#res = "";
+#for c in str:
+#    if c.isupper():
+#        res += c.lower()
+#    else:
+#        res += c.upper()
+#print(res)
+
+#One liner solution
+res2 = str.swapcase();
+print(res2);
