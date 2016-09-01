834509e4ff275f769de3662a3b92f01d6dbbd611
Mayank Garg <mayank11065@iiitd.ac.in>
2016-09-01 11:53:31 +0530

    No Idea!

---
+++ Python/Sets/NoIdea.py
@@ -0,0 +1,13 @@
+input()
+N = list(map(int,input().split()))
+A = set(map(int,input().split()))
+B = set(map(int,input().split()))
+
+happiness = 0
+for i in N:
+    if i in A:
+        happiness += 1
+    elif i in B:
+        happiness -= 1
+
+print(happiness)
